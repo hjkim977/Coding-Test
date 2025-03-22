61268109cbc6e30e6c7cd2754c88f743738aa419
HyeJi <eoegog@naver.com>
2025-03-22 16:05:58 +0900

    [level 2] Title: 기능개발, Time: 0.04 ms, Memory: 9.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -0,0 +1,24 @@
+def solution(pro, speeds):
+    answer = []
+    days = []
+    queue=[]
+    
+    for i in range(len(pro)):
+        remain=100-pro[i]
+        if remain%speeds[i]==0:
+            days.append(remain//speeds[i])
+        else:
+            days.append(remain//speeds[i]+1)
+
+    queue.append(days[0])
+
+    for i in range(1,len(days)):
+        if len(queue)==0 or queue[0]>=days[i]:
+            queue.append(days[i])
+        else:
+            answer.append(len(queue))
+            queue.clear()
+            queue.append(days[i])       
+    answer.append(len(queue))
+                           
+    return answer
