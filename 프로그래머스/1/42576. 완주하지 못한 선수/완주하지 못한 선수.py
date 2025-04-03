e3aa65ff8fb224f0b0ca540463893a0fb432067c
HyeJi <eoegog@naver.com>
2025-04-03 11:30:57 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 43.41 ms, Memory: 33.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -0,0 +1,16 @@
+def solution(participant, completion):
+    hash={} #해시테이블 설정, 딕셔너리로 해시를 표현
+    
+    for p in participant:
+        if p in hash:
+            hash[p]+=1
+        else:
+            hash[p]=1
+    
+    for c in completion:
+        if c in hash:
+            hash[c]-=1
+    
+    for key,value in hash.items():
+        if value>0:
+            return key
