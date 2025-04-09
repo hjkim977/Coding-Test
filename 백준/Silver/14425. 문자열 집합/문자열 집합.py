8373cb2399bfc7dd66a24229a81cf5596face6e1
HyeJi <eoegog@naver.com>
2025-04-09 15:28:56 +0900

    [Silver IV] Title: 문자열 집합, Time: 584 ms, Memory: 37432 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.py
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.py
@@ -1,9 +1,9 @@
 n,m=map(int, input().split())
-s=[input() for _ in range(n)] #s에 포함되어 있는 문자열
-w=[input() for _ in range(m)] #검사해야 하는 문자열
+s={input() for _ in range(n)}
 c=0
 
-for i in w:
+for _ in range(m):
+    i=input()
     if i in s:
         c+=1
 print(c)
