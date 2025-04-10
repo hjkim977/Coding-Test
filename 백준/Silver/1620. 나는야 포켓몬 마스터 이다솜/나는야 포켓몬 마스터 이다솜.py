129b036512be4672e051c7c85f0c66b0de3fabdc
HyeJi <eoegog@naver.com>
2025-04-10 15:57:09 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 4988 ms, Memory: 56984 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
@@ -3,13 +3,13 @@ key_name = {}
 key_num = {}
 
 for i in range(1, n + 1):
-    a = input().strip()  # 여기 strip() 붙이기!
+    a = input()
     key_name[a] = i
     key_num[i] = a
 
 result = []
 for _ in range(m):
-    q = input().strip()  # 여기도 strip() 붙이기!
+    q = input()
     if q.isdigit():
         result.append(key_num[int(q)])
     else:
