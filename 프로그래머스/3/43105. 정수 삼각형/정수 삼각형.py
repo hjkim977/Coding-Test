f03e66e4871fa7dd57fffba1e6ce946f95497a4d
HyeJi <eoegog@naver.com>
2025-07-28 16:52:12 +0900

    [level 3] Title: 정수 삼각형, Time: 40.87 ms, Memory: 13.9 MB -BaekjoonHub

---
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
@@ -0,0 +1,10 @@
+def solution(t):
+    for i in range(1,len(t)):
+        for j in range(i+1):
+            if j==0:
+                t[i][j] += t[i-1][j]
+            elif j==i:
+                t[i][j] += t[i-1][j-1]
+            else:
+                t[i][j] += max(t[i-1][j], t[i-1][j-1])
+    return max(t[len(t)-1])
