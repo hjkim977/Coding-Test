c0263abbdfdf3352f2573bd369da9e6380a7e513
HyeJi <eoegog@naver.com>
2025-10-13 14:21:35 +0900

    [level 1] Title: 폰켓몬, Time: 0.74 ms, Memory: 9.88 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,4 +1,6 @@
 def solution(nums):
-    monster=len(set(nums))
-    num=len(nums)//2 #가져가도 되는 개수
-    return min(monster, num)
+    num = len(set(nums))
+    if len(nums)//2<=num:
+        return len(nums)//2
+    else:
+        return num
