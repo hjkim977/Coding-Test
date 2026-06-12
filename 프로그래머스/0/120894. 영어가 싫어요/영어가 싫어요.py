496f86eba037a59ed6b360d67cbe123f49737326
HyeJi <eoegog@naver.com>
2026-06-12 14:28:22 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.01 ms, Memory: 11.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.py
@@ -0,0 +1,6 @@
+def solution(numbers):
+    for word, num in {'one':'1', 'two':'2', 'three':'3', 
+           'four':'4', 'five':'5', 'six':'6', 'seven':'7', 'eight':'8', 'nine':'9',
+            'zero':'0'}.items():
+        numbers = numbers.replace(word.lower(), num)
+    return int(numbers)
